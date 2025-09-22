d38607fa8893e7b656737090eac59b6a956fc5ef
SonuKumar057 <sonukumar6204648@gmail.com>
2025-09-23 00:41:51 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
+app.get("/",(req,res)=>{
+  res.send("App is working");
+})
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
